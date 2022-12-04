5dc43cfb8f77302036ddca5010b1588321dfb563
Christian Koranteng <korantengchristian@gmail.com>
2022-12-04 12:23:52 +0000

    Create SQL scripts

---
+++ setup_mySQL_dev.sql
@@ -0,0 +1,15 @@
+
+-- Create a database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create a user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant privileges
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges
+FLUSH PRIVILEGES;

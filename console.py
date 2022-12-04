59d43997e59a8c5f7ae9509eff011fdbb12a762b
Christian Koranteng <korantengchristian@gmail.com>
2022-12-04 18:44:18 +0000

    update console

--- console.py
+++ console.py
@@ -121,10 +121,11 @@ class HBNBCommand(cmd.Cmd):
         elif args not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        else:
+            new_instance = HBNBCommand.classes[args]()
+            storage.save()
+            print(new_instance.id)
+            storage.save()
 
     def help_create(self):
         """ Help information for the create method """
